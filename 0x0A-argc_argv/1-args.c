ac660952870e2286246d712f82e2bf2873d44d4a
Lynn-swony <merrylinecheruto@gmail.com>
2023-04-16 23:30:11 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-#include "main.h"
+#include <stdlib.h>
 
 /**
- * main - prints the number of arguments passed to it.
- * @argc: the size of argv
- * @argv: pointers to an array of characters
- *
- * Return: EXIT_SUCCESS
- */
+* main - entry point
+* @argc: size of argv
+* @argv: array
+* Return: 0
+**/
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d\n", argc - 1);
-	return (EXIT_SUCCESS);
+	if (argc > 0)
+		printf("%d\n", argc - 1);
+	return (0);
 }
