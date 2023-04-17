18151aae22892e2322b19e895c7533acac8cbb4d
Lynn-swony <merrylinecheruto@gmail.com>
2023-04-16 23:42:54 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,26 +1,15 @@
 #include <stdio.h>
-#include "main.h"
+#include <stdlib.h>
 
 /**
- * main - multiplies two numbers.
- * @argc: argument count
- * @argv: arguments
+ * main - prints the number of arguments passed to it.
+ * @argc: the size of argv
+ * @argv: pointers to an array of characters
  *
- * Return: 0
+ * Return: EXIT_SUCCESS
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	int x, y;
-
-	if (argc < 3)
-	{
-		printf("Error\n");
-		return (1);
-	}
-
-	x = atoi(argv[1]);
-	y = atoi(argv[2]);
-	printf("%d\n", x * y);
-
-	return (0);
+	printf("%d\n", argc - 1);
+	return (EXIT_SUCCESS);
 }
